cc12b06ee2cbca720dcc373e58e5c6bc442701a6
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-01-01 18:28:37 +0600

    install and import jwt and cookie-parser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
